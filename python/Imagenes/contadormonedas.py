8e4a0204f6966531cdead02ad4f0ad3b4d90a4ce
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-01-20 19:41:25 -0500

    a

--- python/Imagenes/contadormonedas.py
+++ python/Imagenes/contadormonedas.py
@@ -1,19 +1,24 @@
-import cv2 as cv2
+from cv2 import cv2
 import numpy as np
 
-valorGauss=3
-valorKernel=3
+valorGauss=1
+valorKernel=7
 original=cv2.imread('monedas.jpg')
 gris=cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
-gauss=cv2.GaussianBlur(gris,(valorGauss,valorGauss),0)
-canny=cv2.Canny(gauss,60,100)
-
+gauss=cv2.GaussianBlur(gris, (valorGauss,valorGauss), 0)
+canny=cv2.Canny(gauss, 60,100)
 kernel=np.ones((valorKernel,valorKernel),np.uint8)
-cierre=cv2.morphologyEx(canny,cv2.MORPH_CLOSE,kernel)
+cierre=cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
 
-contornos,jerarquia=cv2.findContours(cierre.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-print("Monedas encontradas: {}".format(len(contornos)))
+contornos, jerarquía=cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+print("monedas encontradas: {}".format(len(contornos)))
 cv2.drawContours(original, contornos, -1, (0,0,255),2)
-# MOSTRAR IMAGENES
-cv2.imshow('Canny',original)
+#Mostrar resultados
+cv2.imshow("Grises",gris)
+cv2.imshow("gauss",gauss)
+cv2.imshow("canny",canny)
+cv2.imshow("cierre",cierre)
+
+cv2.imshow("Resultado", original)
 cv2.waitKey(0)
--- python/Imagenes/mostrarcamara.py
+++ python/Imagenes/mostrarcamara.py
@@ -1,29 +1,14 @@
 import cv2 as cv
-import numpy as np
-
-capturavideo=cv.VideoCapture(0)
-total=0
-for i in range(101 ):
-    total=i
-    print(f"Procesando imagen... {total}%")
-valorGauss=9
-valorKernel=9
-if not capturavideo.isOpened():
-    print("No se ha encontrado ninguna camara")
+capturaVideo=cv.VideoCapture(0)
+if not capturaVideo.isOpened():
+    print("No se encontro una camara")
     exit()
 while True:
-    tipocamara,camara=capturavideo.read()
-    grises=cv.cvtColor(camara,cv.COLOR_BGR2GRAY)
-    gauss=cv.GaussianBlur(grises,(valorGauss,valorGauss),0)
-    canny=cv.Canny(gauss,60,100)
-    
-    kernel=np.ones((valorKernel,valorKernel),np.uint8)
-    cierre=cv.morphologyEx(canny,cv.MORPH_CLOSE,kernel)
-    
-    contornos,jerarquia=cv.findContours(cierre.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
-    cv.drawContours(camara, contornos, -1, (0,0,255),3) 
-    cv.imshow('Camara', camara)
+    tipocamara,Camara=capturaVideo.read()
+    grises=cv.cvtColor(Camara, cv.COLOR_BGR2GRAY)
+
+    cv.imshow("En vivo",grises)
     if cv.waitKey(1)==ord("q"):
         break
-capturavideo.release()
+capturaVideo.release()
 cv.destroyAllWindows()
