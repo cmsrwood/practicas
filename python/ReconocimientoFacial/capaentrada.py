8a9c0c9ea255b91ce53b2f81d5e0438b6323b7c8
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-01-25 23:40:21 -0500

    a

--- python/ReconocimientoFacial/capaentrada.py
+++ python/ReconocimientoFacial/capaentrada.py
@@ -1,3 +1,7 @@
 import cv2 as cv
 ruidos=cv.CascadeClassifier('C:/Users/Dell/Documents/GitHub/practicas/python/ReconocimientoFacial\haarcascade_frontalface_default.xml')
-print(ruidos)
+camara=cv.VideoCapture(0)
+while True:
+    _,captura=camara.read()
+    grises=cv.cvtColor(captura,cv.COLOR_BGR2GRAY)
+    cara=ruidos.detectMultiScale(grises,)
