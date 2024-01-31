46d0b7dfc1b501f802bfbda9c78d58450a600f0b
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-01-31 01:01:02 -0500

    capa oculta

--- python/ReconocimientoFacial/capaocultaentrenamiento.py
+++ python/ReconocimientoFacial/capaocultaentrenamiento.py
@@ -2,32 +2,34 @@ import cv2 as cv
 import os
 import numpy as np
 from time import time
-dataruta='C:/Users/Dell/Documents/GitHub/practicas/python/ReconocimientoFacial/data'
-listadata=os.listdir(dataruta)
+dataRuta='C:/Users/Dell/Documents/GitHub/practicas/python/ReconocimientoFacial/data'
+listaData=os.listdir(dataRuta)
+#print('data',listaData)
 ids=[]
 rostrosData=[]
 id=0
-tiempoinicial=time()
-for fila in listadata:
-    rutacompleta=dataruta+'/'+fila
+tiempoInicial=time()
+for fila in listaData:
+    rutacompleta=dataRuta+'/'+ fila
     print('Iniciando lectura...')
     for archivo in os.listdir(rutacompleta):
-        
-        print('Imagenes',fila + '/' + archivo)
-        
-        ids.append(id)
-        rostrosData.append(cv.imread(rutacompleta+'/'+archivo,cv.COLOR_BGR2GRAY))
-        
-    id+=1
-    tiempofinaldelectura=time()
-    tiempototaldelectura=tiempofinaldelectura-tiempoinicial
-    print('Tiempo total de lectura: ' , tiempototaldelectura)
+       
+        print('Imagenes: ',fila +'/'+archivo)
     
-entrenamientoEigenRecognizer=cv.face.EigenFaceRecognizer_create()
-print('Iniciando el entrenamiento...espere', entrenamientoEigenRecognizer)
-entrenamientoEigenRecognizer.train(rostrosData,np.array(ids))
-tiempofinalentrenamiento=time()
-tiempototalentrenamiento=tiempofinalentrenamiento-tiempototaldelectura
-print('Tiempo de entrenamiento total: ',tiempototalentrenamiento)
-entrenamientoEigenRecognizer.write('EntrenamientoEigenRecognizer.xml')
+        ids.append(id)
+        rostrosData.append(cv.imread(rutacompleta+'/'+archivo,0))  
+      
+
+    id=id+1
+    tiempofinalLectura=time()
+    tiempoTotalLectura=tiempofinalLectura-tiempoInicial
+    print('Tiempo total lectura: ',tiempoTotalLectura)
+
+entrenamientoEigenFaceRecognizer=cv.face.EigenFaceRecognizer_create()
+print('Iniciando el entrenamiento...espere')
+entrenamientoEigenFaceRecognizer.train(rostrosData,np.array(ids))
+TiempofinalEntrenamiento=time()
+tiempoTotalEntrenamiento=TiempofinalEntrenamiento-tiempoTotalLectura
+print('Tiempo entrenamiento total: ',tiempoTotalEntrenamiento)
+entrenamientoEigenFaceRecognizer.write('EntrenamientoEigenFaceRecognizer.xml')
 print('Entrenamiento concluido')
