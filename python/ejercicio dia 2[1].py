12a40a22d4d1d24e01b520e2ff92f975385e0f4c
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-02-16 15:53:17 -0500

    Add files via upload

---
+++ python/ejercicio dia 2[1].py
@@ -0,0 +1,14 @@
+i=8
+cont =0
+while i>0:
+    i-=1
+    cont+=1
+    print(i)
+print(f"Numero de iteraciones {cont}")
+
+lista=[]
+for i in range(0,11):
+    if(i%2==0):
+        lista.append(i)
+print(lista)
+        
---
+++ python/ejercicio.py
@@ -0,0 +1,5 @@
+nombre=''
+while nombre != 'carlos':
+    nombre=input("Escriba un nombre: ")
+print("Te encontramos carlos :D")
+
