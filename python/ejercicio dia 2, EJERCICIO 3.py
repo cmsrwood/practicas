0eea5a8e0f5e90c10bbe61e5a84292359036d90a
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-02-16 18:08:56 -0500

    Add files via upload

---
+++ python/ejercicio dia 2, EJERCICIO 3.py
@@ -0,0 +1,10 @@
+c=float(input("Ingrese el valor del capital: "))
+c_doble=c*2
+print(f"El valor doble es: {c_doble}")
+meses=0
+while c<c_doble:  
+    i=c*0.05
+    c+=i
+    meses+=1
+    print(f"Mes #{meses}")
+    print(c)
