9c9bcc4cd4cc8539b1b41cd459d3249a47460deb
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-02-23 01:01:28 -0500

    java

---
+++ java/JAVA_UDEMY/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String args[]){
+        int miVariableEntera=10;                                                
+        String hola="Hola";
+        System.out.print(miVariableEntera);
+        System.out.print(hola);
+    }
+}
---
+++ java/JAVA_UDEMY/concatenacion.java
@@ -0,0 +1,13 @@
+public class concatenacion {
+    public static void main(String args[]){
+        var usuario = "Juan";
+        var titulo = "Ingeniero";
+        var union= titulo+" "+usuario;
+        System.out.println(union);
+
+        var i = 3;
+        var j = 4; 
+        System.out.println(i+j);
+
+    }
+}
