429837bb745759695482877ac9925c9a96fed691
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-03-24 01:46:32 -0500

    react

--- React&SpringBoot/recursos-humanos-spring/src/main/java/gm/rh/repositorio/EmpleadoRepositorio.java
+++ React&SpringBoot/recursos-humanos-spring/src/main/java/gm/rh/repositorio/EmpleadoRepositorio.java
@@ -1,8 +1,2 @@
-package gm.rh.repositorio;
-
-import gm.rh.modelo.Empleado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class EmpleadoRepositorio extends JpaRepository <Empleado,Integer> {
-
+package gm.rh.repositorio;public interface EmpleadoRepositorio {
 }
