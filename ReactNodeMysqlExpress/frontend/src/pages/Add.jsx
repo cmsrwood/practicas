3d0ea906132e00709dc33eee01ddd930d05a087a
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-05-18 22:20:53 -0500

    añadir desde react

--- ReactNodeMysqlExpress/frontend/src/pages/Add.jsx
+++ ReactNodeMysqlExpress/frontend/src/pages/Add.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react'
 
 export default function Add() {
+
+const [book,setBook] = useState({
+  title:"",
+  desc:"",
+  cover:"",
+  price:""
+})
+
+const navigate = useNavigate()
+
+const handleChange = (e) =>{
+  setBook(prev=>({...prev,[e.target.name]:e.target.value}))
+}
+
+const handleClick = async (e) =>{
+  e.preventDefault()
+  try{
+    await axios.post("http://localhost:8800/books",book)
+    navigate("/")
+  }catch(err){
+    console.log(err)
+  }
+}
   return (
-    <div>Add</div>
+    <div className='form'>
+      <h1>Add New Book</h1>
+      <form>
+        <input type="text" placeholder='title' onChange={handleChange} name='title'/>
+        <input type="text" placeholder='desc' onChange={handleChange} name='desc'/>
+        <input type="number" placeholder='price' onChange={handleChange} name='price'/>
+        <input type="text" placeholder='cover' onChange={handleChange} name='cover'/>
+        <button onClick={handleClick}>Add</button>
+      </form>
+    </div>
   )
 }
