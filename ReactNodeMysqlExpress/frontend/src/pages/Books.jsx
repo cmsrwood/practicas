695a061a72567e7c77ddc4217fb5f6b5d834eefd
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-05-18 21:41:37 -0500

    usando axios para consultas

--- ReactNodeMysqlExpress/frontend/src/pages/Books.jsx
+++ ReactNodeMysqlExpress/frontend/src/pages/Books.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Books() {
+
+  const [books,setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try{
+        const res= await axios.get('http://localhost:8800/books')
+        console.log(res.data)
+      }catch(err){
+        
+      }
+    }
+    fetchAllBooks()
+  }, [])
+
   return (
     <div>Books</div>
   )
