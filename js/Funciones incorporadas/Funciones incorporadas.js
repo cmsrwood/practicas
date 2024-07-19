2dd46ca7f0f823e54396f8124d9c352708f85bc0
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-07-18 21:13:55 -0500

    funciones incorporadas

--- js/Funciones incorporadas/Funciones incorporadas.js
+++ js/Funciones incorporadas/Funciones incorporadas.js
@@ -1,2 +1,12 @@
 let cadena1='Hola'
 console.log(cadena1.length)
+
+cadena1 = 'x'
+console.log (cadena1)
+
+cadena1 = 'Adios'
+console.log (cadena1)
+
+for (let i=0; i< cadena1.length; i++){
+    console.log(`${i} = ${cadena1[i]}`)
+}
