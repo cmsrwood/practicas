7c8c7ae5663cc95f3069cde76e698e3b6d051ca5
cmsrwood <114179857+cmsrwood@users.noreply.github.com>
2025-02-12 09:00:18 -0500

    Update main.py

--- python/Sena/5to Trimestre/main.py
+++ python/Sena/5to Trimestre/main.py
@@ -1,68 +1,60 @@
 def entradas():    
     
     nombre = str(input("Ingrese su nombre: "))
-    dia_nacimiento = int(input("Ingrese su dia de nacimiento: "))
-    mes_nacimiento = int(input("Ingrese su mes de nacimiento: "))
+
     ano_nacimiento = int(input("Ingrese su año de nacimiento: "))
+    while ano_nacimiento < 1900 or ano_nacimiento >= 2026:
+        print("Año de nacimiento inválido")
+        ano_nacimiento = int(input("Ingrese su año de nacimiento: "))
+
+    mes_nacimiento = int(input("Ingrese su mes de nacimiento: "))
+    while mes_nacimiento < 1 or mes_nacimiento > 12:
+        print("Mes de nacimiento inválido")
+        mes_nacimiento = int(input("Ingrese su mes de nacimiento: "))
+
+
+    dia_nacimiento = int(input("Ingrese su dia de nacimiento: "))
+    while dia_nacimiento < 1 or dia_nacimiento > 31:
+        print("Dia de nacimiento inválido")
+        dia_nacimiento = int(input("Ingrese su dia de nacimiento: "))
+
+    ano_actual = int(input("Ingrese el año actual: "))
+    while ano_actual < 1900 or ano_actual > 2025:
+        print("Año actual inválido")
+        ano_actual = int(input("Ingrese el año actual: "))
+
+
+    mes_actual = int(input("Ingrese el mes actual: "))
+    while mes_actual < 1 or mes_actual > 12:
+        print("Mes actual inválido")
+        mes_actual = int(input("Ingrese el mes actual: "))
+
 
     dia_actual = int(input("Ingrese el dia actual: "))
-    mes_actual = int(input("Ingrese el mes actual: "))
-    ano_actual = int(input("Ingrese el año actual: "))
-    
-    if dia_nacimiento < 1 or dia_nacimiento > 31:
-        print("Dia inválido")
-    if mes_nacimiento < 1 or mes_nacimiento > 12:
-        print("Mes inválido")
-    if dia_actual < 1 or dia_actual > 31:
-        print("Dia inválido")
-    if mes_actual < 1 or mes_actual > 12:
-        print("Mes inválido")
-    if ano_actual < 1900 or ano_actual > 2025:
-        print("Año inválido")
-    if ano_nacimiento < 1900 or ano_nacimiento > 2025:
-        print("Año inválido")
+    while dia_actual < 1 or dia_actual > 31:
+        print("Dia actual inválido")
+        dia_actual = int(input("Ingrese el dia actual: "))
         
-    return dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual
-    
-def mesANombre(mes):
-    switcher = {
-        1: "Enero",
-        2: "Febrero",
-        3: "Marzo",
-        4: "Abril",
-        5: "Mayo",
-        6: "Junio",
-        7: "Julio",
-        8: "Agosto",
-        9: "Septiembre",
-        10: "Octubre",
-        11: "Noviembre",
-        12: "Diciembre"
-    }
-    
-    return switcher.get(mes)
+    return nombre, dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual
 
-def calcular_edad(dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual):
+def calcular_edad(nombre, dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual):
     dia = dia_actual - dia_nacimiento
     meses = mes_actual - mes_nacimiento
     ano = ano_actual - ano_nacimiento
         
-    if meses < 0:
+    if meses <= 0:
         meses += 12
         ano -= 1
             
-    if dia < 0:
+    if dia <= 0:
         dia += 30
         meses -= 1
-        if meses < 0:
-            meses += 12
-            ano -= 1
-    return f"Su edad es: {ano} años, {meses} meses y {dia} días"
+
+    return f"{nombre}, su edad es: {ano} años, {meses} meses y {dia} días"
 
 def main ():
-    dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual = entradas()
-    print(calcular_edad(dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual))
-    
+    nombre, dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual = entradas()
+    print(calcular_edad(nombre, dia_nacimiento, mes_nacimiento, ano_nacimiento, dia_actual, mes_actual, ano_actual))
 
 if __name__ == "__main__":
     main()
